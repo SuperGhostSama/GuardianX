d62e6419d83c9fd79a306f145ef220cb29d35929
Salah <salaheddinehanaoui@gmail.com>
2024-02-13 09:41:36 +0100

    tested interceptor and its working

--- src/app/components/dashboard/role/role.component.ts
+++ src/app/components/dashboard/role/role.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RoleResponseDTO } from 'src/app/dto/responses/role-response.dto';
+import { RoleService } from 'src/app/services/role/role.service';
 
 @Component({
   selector: 'app-role',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./role.component.css']
 })
 export class RoleComponent {
+  roles: RoleResponseDTO[] = [];
 
+  constructor(private roleService: RoleService) { }
+
+  ngOnInit(): void {
+    this.loadRoles();
+  }
+
+  loadRoles() {
+    this.roleService.getAllRoles().subscribe(
+      (roles: RoleResponseDTO[]) => {
+        this.roles = roles;
+        console.log('Roles:', roles);
+      },
+      (error) => {
+        console.error('Error fetching roles:', error);
+      }
+    );
+  }
 }
