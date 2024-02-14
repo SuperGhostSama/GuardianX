de86a43c064e2fca9de3906c801a346264b89605
Salah <salaheddinehanaoui@gmail.com>
2024-02-14 08:47:22 +0100

    finished register

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -15,10 +16,22 @@ export class RegisterComponent {
   private ngUnsubscribe: Subject<void> = new Subject<void>();
 
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {}
 
   onSubmit() {
+    this.authService.register({ name: this.name, email: this.email, password: this.password })
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(
+        (registrationResponse) => {
+            console.log('Registration successful:', registrationResponse);
+            this.router.navigate(['/login']);
+        },
+        (error) => {
+          console.error('Registration failed:', error);
+        }
+      );
 
   }
 
